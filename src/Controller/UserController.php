c92177d32b10b6d92adabcdb2caadfa57c800b21
FEUGANG KOUAKEM Ange Corianne <afeugangkouake@et.intechinfo.fr>
2024-03-30 16:35:39 +0100

    Evenlistener error token

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,12 +6,16 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManager;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\InsufficientAuthenticationException;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
@@ -287,37 +291,37 @@ class UserController extends AbstractController
     #[Route('/user/{id}', name: 'user_delete', methods: ['DELETE'])]
     public function delete(Request $request): JsonResponse
     {
-        $user = $this->repository->find($request->get('id'));
-        if(!$user) {
+            $user = $this->repository->find($request->get('id'));
+            if(!$user) {
+
+                $data = $this->serializer->serialize(
+                    ['error' => true,'message' => "L'utilisateur n'existe pas"], 
+                    'json'); 
+                
+                return new JsonResponse($data, Response::HTTP_NOT_FOUND, [], true);
+            }
+
+            $this->entityManager->remove($user);
+            $this->entityManager->flush();
 
             $data = $this->serializer->serialize(
-                ['error' => true,'message' => "L'utilisateur n'existe pas"], 
-                'json'); 
+                ['error' => false, 'message' => 'User deleted successefully','data' =>   $user], 
+                'json', 
+                [
+                    'groups' => 'getUsers',
+                    AbstractNormalizer::CALLBACKS => [
+                        'dateBirth' => function ($innerObject, $outerObject, string $attributeName, string $format = null, array $context = []) {
+                            return $innerObject instanceof \DateTimeInterface ? $innerObject->format('d-m-Y') : '';
+                        },
+                        'createAt' => function ($innerObject, $outerObject, string $attributeName, string $format = null, array $context = []) {
+                            return $innerObject instanceof \DateTimeInterface ? $innerObject->format('d-m-Y') : '';
+                        },
+                        'updateAt' => function ($innerObject, $outerObject, string $attributeName, string $format = null, array $context = []) {
+                            return $innerObject instanceof \DateTimeInterface ? $innerObject->format('d-m-Y') : '';
+                        }
+                    ]
+            ]); 
             
-            return new JsonResponse($data, Response::HTTP_NOT_FOUND, [], true);
-        }
-
-        $this->entityManager->remove($user);
-        $this->entityManager->flush();
-
-        $data = $this->serializer->serialize(
-            ['error' => false, 'message' => 'User deleted successefully','data' =>   $user], 
-            'json', 
-            [
-                'groups' => 'getUsers',
-                AbstractNormalizer::CALLBACKS => [
-                    'dateBirth' => function ($innerObject, $outerObject, string $attributeName, string $format = null, array $context = []) {
-                        return $innerObject instanceof \DateTimeInterface ? $innerObject->format('d-m-Y') : '';
-                    },
-                    'createAt' => function ($innerObject, $outerObject, string $attributeName, string $format = null, array $context = []) {
-                        return $innerObject instanceof \DateTimeInterface ? $innerObject->format('d-m-Y') : '';
-                    },
-                    'updateAt' => function ($innerObject, $outerObject, string $attributeName, string $format = null, array $context = []) {
-                        return $innerObject instanceof \DateTimeInterface ? $innerObject->format('d-m-Y') : '';
-                    }
-                ]
-        ]); 
-        
-        return new JsonResponse($data, Response::HTTP_OK, [], true);
+            return new JsonResponse($data, Response::HTTP_OK, [], true);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -239,6 +239,11 @@ class User implements UserInterface,PasswordAuthenticatedUserInterface
         return (string) $this->email;
     }
 
+    public function getUsername(): string
+    {
+        return $this->getUserIdentifier();
+    }
+
     /**
      * @see UserInterface
      */
