cdb816d67b41d9851c98291a8848b396ddad3df0
FEUGANG KOUAKEM Ange Corianne <afeugangkouake@et.intechinfo.fr>
2024-04-21 17:09:36 +0200

    update artist

--- migrations/Version20240421145300.php
+++ migrations/Version20240421145300.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240419121025 extends AbstractMigration
+final class Version20240421145300 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20240419121025 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE album ADD active TINYINT(1) NOT NULL');
-        $this->addSql('ALTER TABLE song ADD active TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE artist ADD avatar VARCHAR(150) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE album DROP active');
-        $this->addSql('ALTER TABLE song DROP active');
+        $this->addSql('ALTER TABLE artist DROP avatar');
     }
 }
